9c55fb51dc9085ecc994653884cd6de5bf501207
Khalid-Elkhidir <elkhidir.khalid@gmail.com>
2023-11-03 16:11:50 -0700

    hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    for str in dir(hidden_4):
+        if str[0] == '_':
+            continue
+        else:
+            print(str)
