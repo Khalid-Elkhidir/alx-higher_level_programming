1ea45f97e255b200066628ce85cb3e697ae3c532
Khalid-Elkhidir <elkhidir.khalid@gmail.com>
2023-11-08 11:51:17 -0800

    Square matrix simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+	 """
+   	 function that computes the square
+  	 value of all integers of a matrix.
+  	 """
 	new_matrix = []
 	for i in matrix:
 		new_sub_array = []
